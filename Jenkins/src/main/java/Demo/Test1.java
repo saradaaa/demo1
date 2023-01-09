708b6e35b53aaa7a81db4766c992342ec424b818
HP <HP@DESKTOP-4MMP2GL>
2023-01-09 16:50:02 +0530

    first commit

---
+++ Jenkins/src/main/java/Demo/Test1.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+public static void main(String[] args) 
+{
+	System.out.println("welcome");
+}
+	
+}
---
+++ Jenkins/src/main/java/Demo/Test2.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+public static void main(String[] args)
+{
+System.out.println("hiiiii");	
+}
+}
